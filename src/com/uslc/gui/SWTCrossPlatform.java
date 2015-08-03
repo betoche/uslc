9545983bb1f3086e97a6eba055343bf04e4a922e
Alberto Larios S <betoche_1@localhost.localdomain>
2015-08-03 06:07:51 -0600

    Steps to follow

---
+++ src/com/uslc/gui/SWTCrossPlatform.java
@@ -0,0 +1,14 @@
+package com.uslc.gui;
+
+public class SWTCrossPlatform {
+
+	public static void main(String[] args) {
+		/*
+		 * 1 - Find the OS version
+		 * 2 - Creates the ant file
+		 * 3 - Execute the ant file
+		 */
+		
+	}
+
+}
